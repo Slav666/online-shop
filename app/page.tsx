00facbdd641e3f0cb07e2e6111f9fd43a615ab9d
Slawomir Dyk <slawomir.dyk@astrosat.net>
2025-01-17 20:45:02 +0000

    add a carousel image using swiper library

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,13 @@
 "use client";
 import ProductList from "./about/productList";
 import products from "./data/product";
+import Carousel from "./swiper";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
-      {/* Hero Section */}
-      <section
-        className="bg-[#2E8B57] text-white h-96 bg-cover bg-center"
-        style={{ backgroundImage: "url('0.jpg')" }}
-      >
-        <div className="flex justify-center items-center h-full bg-opacity-50">
-          {/* <Image
-            src="/0.jpg"
-            alt=""
-            layout="responsive"
-            objectFit="cover"
-            height={100}
-            width={100}
-          /> */}
-        </div>
-      </section>
+    <div>
+      <Carousel />
+  
 
       {/* Featured Products Section */}
       <section className="py-12 bg-gray-100">
